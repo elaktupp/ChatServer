ef1e254ca45a68ce1ea19f8ab4d5e5cb40b14e6c
Ohjelmistokehitys <Ohjelmistokehitys@OpiFrame14-Th>
2014-11-28 13:46:13 +0200

    Disconnect need rework

--- src/chatserver/ChatServer.java
+++ src/chatserver/ChatServer.java
@@ -9,9 +9,8 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.HashMap;
-
-import message.ChatMessage; // our very own
+import message.Chat;
+import message.ChatUserListUpdate;
 
 /**
  *
@@ -30,7 +29,7 @@ public class ChatServer {
             ServerSocket server = new ServerSocket(3010);
             // instead of hard coded port this could for example
             // read the value from a configuration file, etc.
-            
+            System.out.println("SERVER: Online");            
             // Start to listen and wait connections
             while(true) {
                 // Waits here for a client to connect (i.e. creates socket)
@@ -43,29 +42,68 @@ public class ChatServer {
         }
     }
     
-    public static void broadcastMessage(ChatMessage msg) {
+    public static void broadcastMessage(Object obj) {
+        
+        System.out.println("SERVER: broadcastMessage "+obj.toString());
         
         // Pass message to all connected clients
         for (ServerClientBackEnd it : clients) {
-            it.sendMessage(msg);
+            it.sendMessage(obj);
         }        
     }
     
     public static void addClient(Socket s) {
         ServerClientBackEnd backEnd = new ServerClientBackEnd(s);
-        // TESTING
+
         System.out.println("SERVER: Add client "+backEnd);
         clients.add(backEnd);
+        
         Thread t = new Thread(backEnd);
         // Tell JVM this is background thread, so JVM can kill
         // it when backEnd is destroyed.
         t.setDaemon(true);
         t.start();
+
+        //sendClientList();
     }
     
     public static void removeClient(ServerClientBackEnd client) {
         // TESTING
         System.out.println("SERVER: Remove client "+client);
-        clients.remove(client);
+        System.out.println(clients.remove(client));
+        
+        sendClientList();
+    }
+    
+    public static void sendClientList() {
+        System.out.println("SERVER: Send list of clients");
+        ArrayList<String>names = new ArrayList();
+        for (ServerClientBackEnd it : clients) {
+            names.add(it.getUserName());
+        }   
+        broadcastMessage(new ChatUserListUpdate(names));
+    }
+    
+    public static String validateUserName(String name) {
+        int nameModifier = 0;
+        String candidate = name;
+        boolean again = false;
+
+        do {
+            again = false;
+            for (ServerClientBackEnd it : clients) {
+                String itName = it.getUserName();
+                if (itName != null && itName.equals(candidate)) {
+                    nameModifier++;
+                    candidate = name + Integer.toString(nameModifier);
+                    again = true;
+                    break;
+                }
+            }
+        } while (again);
+        
+        System.out.println("SERVER: Validate name "+name+" -> "+candidate);
+        
+        return candidate;
     }
 }
--- src/chatserver/ServerClientBackEnd.java
+++ src/chatserver/ServerClientBackEnd.java
@@ -10,7 +10,9 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.SocketException;
-import message.Chat;
+import message.ChatConnect;
+import message.ChatConnectResponse;
+import message.ChatDisconnect;
 
 import message.ChatMessage; // our very own
 
@@ -23,6 +25,7 @@ public class ServerClientBackEnd implements Runnable {
     private final Socket socket;
     private ObjectInputStream input;
     private ObjectOutputStream output;
+    private String userName;
     
     public ServerClientBackEnd(Socket socket) {
         this.socket = socket;
@@ -36,8 +39,9 @@ public class ServerClientBackEnd implements Runnable {
         
             // Waits for data from clients
             while(true) {
-                ChatMessage msg = (ChatMessage) input.readObject();
-                ChatServer.broadcastMessage(msg);
+
+                messageHandler(input.readObject());
+                
             }
         } catch (SocketException ex) {
             try {
@@ -53,12 +57,56 @@ public class ServerClientBackEnd implements Runnable {
         }
     }
     
-    public void sendMessage(ChatMessage msg) {
+    public void sendMessage(Object obj) {
         try {
-            output.writeObject(msg);
+            output.writeObject(obj);
             output.flush();
         } catch (IOException ex) {
             ex.printStackTrace();
+        }
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    private void messageHandler(Object obj) {
+
+        System.out.println("SYSTEM BACK: "+obj.toString());
+        
+        if (obj instanceof ChatMessage) {
+            
+            ChatServer.broadcastMessage(obj);
+            
+        } else if (obj instanceof ChatConnect) {
+            
+            String validName = ChatServer.validateUserName(((ChatConnect)obj).getUserName());
+            setUserName(validName);
+            
+            // Make response message
+            ChatConnectResponse resp = new ChatConnectResponse();
+            resp.setConnected(true);
+            if (((ChatConnect)obj).getUserName().equals(validName)) {
+                resp.setNameChanged(false);
+            } else {
+                resp.setNameChanged(true);
+            }
+            resp.setUserName(validName);
+            sendMessage(resp);
+        } else if (obj instanceof ChatDisconnect) {
+            try {
+                output.close();
+                input.close();
+                socket.close();
+            } catch (IOException ex) {
+                // Nothing we can do...
+                ex.printStackTrace();
+            }
+            ChatServer.removeClient(this);
         }
     }
 
